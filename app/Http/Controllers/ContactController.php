06a794bdf500f1f560d2671e0319ad2cddf4df03
Mwakai <mwakaimwambala@gmail.com>
2024-06-12 18:02:27 +0300

    init

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            "name" => "required|unique:contacts,name,except,id",
+            "email" => "sometimes|unique:contacts,email,except,id",
+            "phone" => "sometimes|unique:contacts,phone,except,id",
+            "address" => "sometimes|unique:contacts,address,except,id",
+        ]);
+
+        $Contact = Contact::create([
+            "name" => $request->name,
+            "email" => $request->email,
+            "phone" => $request->phone,
+            "address" => $request->address,
+        ]);
+        return redirect()->route("dashboard");
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Contact $contact)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Contact $contact)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Contact $contact)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Contact $contact)
+    {
+        //
+    }
+
+    public function contacts()
+    {
+        return Inertia::render('Dashboard', [
+            'contacts' => Contact::with("category")->get(),
+            'message' => session("message")
+        ]);
+    }
+
+    public function create_contact()
+    {
+        return Inertia::render("CreateContact", [
+            
+        ]);
+    }
+
+}
---
+++ resources/js/Pages/CreateContact.vue
@@ -0,0 +1,106 @@
+<script setup>
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, Link, useForm } from '@inertiajs/vue3';
+
+const form = useForm({
+    name: '',
+    email: '',
+    phone: '',
+    address: '',
+});
+
+const submitForm = () => {
+    form.post(route('contact.store'));
+};
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Create Contacts</h2>
+        </template>
+        <div class="py-12">
+            <div class="max-w-3xl py-12 mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="relative py-12 overflow-x-auto shadow-md sm:rounded-lg">
+                        <form
+                            @submit.prevent="submitForm"
+                            class="max-w-md mx-auto"
+                        >
+                            <div class="mb-4">
+                                <label
+                                    for="name"
+                                    class="block text-sm font-medium text-gray-700"
+                                    >Full Name</label
+                                >
+                                <input
+                                    type="text"
+                                    id="name"
+                                    v-model="form.name"
+                                    required
+                                    class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
+                                />
+                            </div>
+                            <div class="mb-4">
+                                <label
+                                    for="email"
+                                    class="block text-sm font-medium text-gray-700"
+                                    >Email</label
+                                >
+                                <input
+                                    type="email"
+                                    id="email"
+                                    v-model="form.email"
+                                    required
+                                    class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
+                                />
+                            </div>
+                            <div class="mb-4">
+                                <label
+                                    for="phone"
+                                    class="block text-sm font-medium text-gray-700"
+                                    >Phone</label
+                                >
+                                <input
+                                    type="tel"
+                                    id="phone"
+                                    v-model="form.phone"
+                                    required
+                                    class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
+                                />
+                            </div>
+                            <div class="mb-4">
+                                <label
+                                    for="phone"
+                                    class="block text-sm font-medium text-gray-700"
+                                    >Address</label
+                                >
+                                <input
+                                    type="text"
+                                    id="address"
+                                    v-model="form.address"
+                                    required
+                                    class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
+                                />
+                            </div>
+                            <div class="flex justify-end">
+                                <button
+                                    type="submit"
+                                    class="bg-blue-500 text-white px-4 py-2 rounded"
+                                >
+                                    Save
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
