1903dd208d48ccc024ac18abffab07bb9b3ea2a4
Mwakai <mwakaimwambala@gmail.com>
2024-06-13 03:53:35 +0300

    chore: cleanup

--- resources/js/Components/GroupForm.vue
+++ resources/js/Components/GroupForm.vue
@@ -16,29 +16,35 @@ const submitForm = () => {
 </script>
 
 <template>
-    <form @submit.prevent="submitForm">
-        <div class="mb-4">
-            <label
-                for="name"
-                class="block text-sm font-medium text-gray-700"
-                >Name</label
-            >
-            <input
-                type="text"
-                id="name"
-                v-model="form.name"
-                required
-                class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
-            />
-        </div>
+    <div class="max-w-3xl py-12 mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Add Group</h2>
+            <br />
+            <form @submit.prevent="submitForm">
+                <div class="mb-4">
+                    <label
+                        for="name"
+                        class="block text-sm font-medium text-gray-700"
+                        >Name</label
+                    >
+                    <input
+                        type="text"
+                        id="name"
+                        v-model="form.name"
+                        required
+                        class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
+                    />
+                </div>
 
-        <div class="flex justify-end">
-            <button
-                type="submit"
-                class="bg-blue-500 text-white px-4 py-2 rounded"
-            >
-                Save
-            </button>
+                <div class="flex justify-end">
+                    <button
+                        type="submit"
+                        class="bg-blue-500 text-white px-4 py-2 rounded"
+                    >
+                        Save
+                    </button>
+                </div>
+            </form>
         </div>
-    </form>
+    </div>
 </template>
