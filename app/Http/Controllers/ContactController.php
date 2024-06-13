b6ba4230282ebf5f2c6d0c3713fa742134d9233b
Mwakai <mwakaimwambala@gmail.com>
2024-06-13 17:51:29 +0300

    feat:add group id

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -9,17 +9,20 @@ use Inertia\Inertia;
 
 class ContactController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        $contacts = Contact::all();
+        $contacts = Contact::with('group')->get();
         $groups = Group::all();
         
         return Inertia::render('Dashboard', compact('contacts', 'groups'));
     }
     
+    
+
+    /**
+     * Show the form for creating a new resource.
+     */
+
 
     /**
      * Show the form for creating a new resource.
@@ -29,9 +32,6 @@ class ContactController extends Controller
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $request->validate([
@@ -47,25 +47,11 @@ class ContactController extends Controller
         return redirect()->route("dashboard");
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Contact $contact)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Contact $contact)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Contact $contact)
     {
         $request->validate([
@@ -81,14 +67,10 @@ class ContactController extends Controller
         return redirect()->route("dashboard");
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Contact $contact)
     {
         $contact->delete();
 
         return redirect()->route("dashboard");
     }
-
 }
--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -14,6 +14,7 @@ class Contact extends Model
         "email",
         "phone",
         "address",
+        "group_id",
     ];
 
     public function group() {
