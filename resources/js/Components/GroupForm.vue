88184b6e75bd0f67cda3308b4e11e8e1150ffaec
Mwakai <mwakaimwambala@gmail.com>
2024-06-13 03:39:57 +0300

    feat: added grouping for contacts

---
+++ resources/js/Components/GroupForm.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref } from 'vue';
+import { useForm } from '@inertiajs/vue3';
+
+const form = useForm({
+    name: '',
+});
+
+const submitForm = () => {
+    form.post('/groups', {
+        onSuccess: () => {
+            form.reset();
+        },
+    });
+};
+</script>
+
+<template>
+    <form @submit.prevent="submitForm">
+        <div class="mb-4">
+            <label
+                for="name"
+                class="block text-sm font-medium text-gray-700"
+                >Name</label
+            >
+            <input
+                type="text"
+                id="name"
+                v-model="form.name"
+                required
+                class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
+            />
+        </div>
+
+        <div class="flex justify-end">
+            <button
+                type="submit"
+                class="bg-blue-500 text-white px-4 py-2 rounded"
+            >
+                Save
+            </button>
+        </div>
+    </form>
+</template>
---
+++ resources/js/Pages/Groups/Index.vue
@@ -0,0 +1,117 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Link, usePage, Head } from '@inertiajs/vue3';
+import { ref } from 'vue';
+import Modal from '@/Components/Modal.vue';
+import GroupForm from '@/Components/GroupForm.vue';
+
+const { props } = usePage();
+const groups = props.groups;
+const showModal = ref(false); // State to manage the visibility of the modal
+
+const closeModal = () => {
+    showModal.value = false;
+};
+
+const openModal = () => {
+    showModal.value = true;
+};
+</script>
+
+<template>
+    <Head title="Groups" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Groups</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="relative overflow-x-auto shadow-md sm:rounded-lg p-4">
+                        <button
+                            @click="openModal"
+                            class="bg-blue-500 hover:bg-blue-700 text-white mb-2 w-42 font-bold py-2 px-4 rounded-full float-right"
+                        >
+                            Create Group
+                        </button>
+
+                        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                            <thead
+                                class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+                            >
+                                <tr>
+                                    <th
+                                        scope="col"
+                                        class="px-6 py-3"
+                                    >
+                                        ID
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        class="px-6 py-3"
+                                    >
+                                        Group Name
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        class="px-6 py-3"
+                                    >
+                                        Total Contacts
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        class="px-6 py-3"
+                                    >
+                                        Action
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr
+                                    v-for="(group, index) in groups"
+                                    :key="group.id"
+                                    class="bg-white border-b dark:bg-gray-900 dark:border-gray-700"
+                                >
+                                    <th
+                                        scope="row"
+                                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                                    >
+                                        {{ group.id }}
+                                    </th>
+                                    <td class="px-6 py-4">{{ group.name }}</td>
+                                    <td class="px-6 py-4">{{ group.contacts_count }}</td>
+                                    <td class="px-6 py-4">
+                                        <Link
+                                            :href="`/groups/${group.id}/edit`"
+                                            class="font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1"
+                                            >Edit</Link
+                                        >
+                                        <Link
+                                            :href="route('groups.destroy', group.id)"
+                                            method="delete"
+                                            as="button"
+                                            class="font-medium text-red-600 dark:text-red-500 hover:underline mx-1"
+                                            onclick="return confirm('Are you sure you want to delete this group?')"
+                                            >Delete</Link
+                                        >
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+
+    <!-- Modal for Creating Group -->
+    <Modal
+        :show="showModal"
+        title="Create Group"
+        @close="closeModal"
+    >
+        <GroupForm @close="closeModal"></GroupForm>
+    </Modal>
+</template>
